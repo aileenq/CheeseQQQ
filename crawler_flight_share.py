762cd628f0bf1f70d72a85e8675d1e53a4086185
CheeseQ <46170148+aileenq@users.noreply.github.com>
2020-07-16 18:31:09 -0400

    Add files via upload

--- crawler_flight_share.py
+++ crawler_flight_share.py
@@ -8,15 +8,15 @@ from datetime import datetime
 
 
 search_dates = ['2020-09-02','2020-09-05','2020-09-09','2020-09-12','2020-09-16','2020-09-19','2020-09-23','2020-09-26'] #你想要搜索的日期
-base_url = 'https://www.google.com/flights?lite=0#flt=SFO.PVG.2020-11-25.SFOPVG0UA857;c:USD;e:1;a:UA;sd:1;t:b;tt:o;sp:2.USD.706410' #这里可以替换成任何你想搜的航班的google flight URL
+base_url = 'https://www.google.com/flights?lite=0#flt=SFO.PVG.2020-11-25.SFOPVG0UA857;c:USD;e:1;a:UA;sd:1;t:b;tt:o;sp:2.USD.706410' #这里可以替换成任何你想搜的航班的google flight url
 
 
 time_out_seconds = 3 # set the time to wait till web fully loaded
-executable_path = '-----你的chromedriver存放路径-----' #例如 ‘/Users/zhangsan/Downloads/chromedriver’
+executable_path = '/Users/zhangsan/Downloads/FlightCrawler/chromedriver' #请把zhangsan替换成你自己mac的用户名
 
 option = webdriver.ChromeOptions()
-option.add_argument('headless')
-driver = webdriver.Chrome('-----你的chromedriver存放路径-----', chrome_options=option) #例如 ‘/Users/zhangsan/Downloads/chromedriver’
+option.add_argument('headless') 
+driver = webdriver.Chrome('/Users/zhangsan/Downloads/FlightCrawler/chromedriver', chrome_options=option) #请把zhangsan改成你自己mac的用户名
 driver.get(base_url)
 
 found = False
@@ -32,17 +32,17 @@ for i in range(0,250): #刷250轮
 		try:
 			driver.find_element_by_class_name('gws-flights-results__error-message')
 			print(datetime.now())
-			print('UA857 SFO-PVG: no result on date: ' + my_date ) #设定没有搜到航班时的提示信息
+			print('UA857 SFO-PVG: no result on date: ' + my_date ) #没有搜到航班的提示信息
 			driver.quit()
 			time.sleep(3)
 			continue
 		except:
-			playsound('-----你的提示音文件路径-----') #播放提示音 文件路径例如‘/Users/zhangsan/Downloads/alert.mp3’
+			playsound('/Users/zhangsan/Downloads/FlightCrawler/found.mp3') #播放发现航班的提示音
 			print('❥△❥ flight search attempted on date ' +  my_date + ' flight found!❥△❥')
 			print('  url:', my_url)
 			found = True
 			break
 	
 print("End of 250 rounds.")
-playsound('-----你的结束提示音文件路径-----') #播放结束提示音 文件路径例如‘/Users/zhangsan/Downloads/end.mp3’
+playsound('/Users/zhangsan/Downloads/FlightCrawler/end.mp3') #播放结束提示音
 driver.quit()  
